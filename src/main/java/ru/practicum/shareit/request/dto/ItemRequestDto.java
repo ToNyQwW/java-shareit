4003bd8cfd1d3175d118738817c6b13bf6371d09
Konstantin <tenenbaum1994@gmail.com>
2025-11-27 16:41:41 +0700

    checkStyle фикс

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -4,13 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
-@EqualsAndHashCode(of = "id")
 public class ItemRequestDto {
 
     private Long id;
