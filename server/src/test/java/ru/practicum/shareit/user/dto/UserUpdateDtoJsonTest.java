40ad2e16ade1419d2d7c248b91a8ec4438770e52
Konstantin <tenenbaum1994@gmail.com>
2026-01-07 18:36:36 +0700

    написаны json тесты для User dto.

---
+++ server/src/test/java/ru/practicum/shareit/user/dto/UserUpdateDtoJsonTest.java
@@ -0,0 +1,31 @@
+package ru.practicum.shareit.user.dto;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class UserUpdateDtoJsonTest {
+
+    private final JacksonTester<UserUpdateDto> json;
+
+    @Test
+    void testDeserialize() throws Exception {
+        String jsonContent = """
+                {
+                  "name": "Updated name",
+                  "email": "updated@test.com"
+                }
+                """;
+
+        UserUpdateDto result = json.parseObject(jsonContent);
+
+        assertThat(result.getName()).isEqualTo("Updated name");
+        assertThat(result.getEmail()).isEqualTo("updated@test.com");
+    }
+}
